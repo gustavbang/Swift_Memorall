0f189b33f1d062ec8d8f7f3803af6cc766dc8a2a
admin <admin@Remys-MacBook-Pro.local>
2019-01-17 15:56:52 +0100

    forView shows correct pic in pins and details + new pins visually changed

--- MapAPP/ViewController.swift
+++ MapAPP/ViewController.swift
@@ -115,13 +115,27 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         annotationView.detailCalloutAccessoryView = description
         annotationView.rightCalloutAccessoryView = UIButton(type: .infoLight)
         
-        // Shows custom picture in pin
-        let detailImageSize = CGSize(width: 150, height: 150)
-        UIGraphicsBeginImageContext(detailImageSize)
-        annotation.image!.draw(in: CGRect(x: 0, y: 0, width: detailImageSize.width, height: detailImageSize.height))
-        let annotionImageResized = UIGraphicsGetImageFromCurrentImageContext()
-        annotationView.leftCalloutAccessoryView = UIImageView(image: annotionImageResized)
+        // Shows custom picture in pin through a couple of steps
+        let imageUrl:URL = URL(string: annotation.imageUrl!)!
         
+        // Start background thread so that image loading does not make app unresponsive
+        DispatchQueue.global(qos: .userInitiated).async {
+            
+            let imageData: NSData = NSData(contentsOf: imageUrl)!
+            
+            // When background task downloaded the data, pin needs to be updated on main_queue
+            DispatchQueue.main.async {
+                let image = UIImage(data: imageData as Data)
+                annotation.image = image
+                
+                let detailImageSize = CGSize(width: 150, height: 150)
+                UIGraphicsBeginImageContext(detailImageSize)
+                annotation.image!.draw(in: CGRect(x: 0, y: 0, width: detailImageSize.width * 0.5, height: detailImageSize.height * 0.5))
+                let annotionImageResized = UIGraphicsGetImageFromCurrentImageContext()
+                annotationView.leftCalloutAccessoryView = UIImageView(image: annotionImageResized)
+            }
+        }
+
         // Makes us able to show extra info, such as image or text
         annotationView.canShowCallout = true
         return annotationView
@@ -181,18 +195,11 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func addAnnotation(name: String, subtitle: String, text: String,
                        picture: UIImage, type: String) {
         if let coordinate = coordinate2D {
-            let annotation = MyAnno(title: name, subtitle: subtitle, coordinate: coordinate)
-            annotation.image = picture
-            annotation.descriptionText = text
-            annotation.type = type
-            mapView.addAnnotation(annotation)
-            
             let latitude = String(coordinate.latitude)
             let longitude = String(coordinate.longitude)
             
-            //her kalder vi storage metode og får et imageURL tilbage
-            //dette imageURL er en reference, som vi også gerne vil gemme i DB
-            
+            // uploadPinImage is called to storage image and returns an imageURL on completion
+            // imageURL is the reference to the pins image and are saved in our database
             uploadPinImage(image: picture) { (imageUrl) in
                 let newPinRef = self.dbRef?.childByAutoId()
                 newPinRef?.setValue(["title": name, "subtitle": subtitle, "latitude": latitude, "longitude": longitude, "imageUrl": imageUrl, "type": type, "descriptionText": text])
@@ -200,41 +207,36 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         }
     }
     
-    
-    
     func uploadPinImage(image: UIImage, completion: @escaping (String) -> Void) {
-        print("1")
         guard let imageData = image.jpegData(compressionQuality: 1) else {
             return
         }
         
-        //unique id
+        // Unique id for picture name
         let imageName = NSUUID.init()
         let storageRef = Storage.storage().reference().child("pinImages").child("\(imageName)")
         
-        //til at få metadata
+        // To get metadata
         let meta = StorageMetadata()
         meta.contentType = "image/png"
         
-        //laver en task
+        // Create our upload task with putData
         let uploadTask = storageRef.putData(imageData, metadata: meta) { (data, error) in
-            
             if error != nil {
-                print(error)
+                print(error!)
                 return
             }
             
+            // Example of downloadUrl result:
+            // https://firebasestorage.googleapis.com/v0/b/memorall.appspot.com/o/pinImages%2F494BFF52-AE02-412F-888C-75AE61EA2415?alt=media&token=9ed8b82b-7dff-402d-bd28-87edd77b7660
             storageRef.downloadURL(completion: { (url, error) in
                 if (error == nil) {
                     if let downloadUrl = url {
-                        // Make you download string
                         let imageUrl = downloadUrl.absoluteString
-                        
-                        
-                        completion(imageUrl)
+                        completion(imageUrl) // "returns" imageUrl as string
                     }
                 } else {
-                    print("fail")
+                    print("Failure", error!)
                 }
             })
             print(data ?? "NO METADATA")
@@ -244,7 +246,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     func loadPins() {
         dbRef?.queryOrdered(byChild: "title").observe(.value, with: { (snapshot) in
-            //tømmer
+            // Empties our annotations array, so it is not dublicated
             self.annotations = [MyAnno]()
             for child in snapshot.children {
                 if let data = child as? DataSnapshot {
@@ -264,36 +266,26 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                     let type = dict["type"] ?? ""
                     let descriptionText = dict["descriptionText"] ?? ""
                 
-                
                     let pin = MyAnno(id: id, title: title, subtitle: subtitle, imageUrl: imageUrl, coordinate: coordinate, type: type, descriptionText: descriptionText)
                     
-                    if pin.image == nil {
-                        pin.image = UIImage(named: "download")
-                    }
-                    
-                    print("ImageURL: ", pin.imageUrl)
-                    //fylder default image
+                    // If imageUrl is not "", then we have a custom picture
                     if (pin.imageUrl != "") {
-
                         let storageRef = Storage.storage().reference(forURL: pin.imageUrl!)
-                        
-                        
                         storageRef.getData(maxSize: 8 * 1024 * 1024, completion: { (data, error) in
                             if let pic = UIImage(data: data!) {
                                 pin.image = pic
                             }
                         })
-                        
-                        
-                        
                     }
                     
+                    // Default image, stored locally in assets
+                    if pin.image == nil {
+                        pin.image = UIImage(named: "download")
+                    }
                     
                     self.annotations.append(pin)
                     self.mapView.addAnnotation(pin)
 
-                    
-                    
                 }
             }
         })
